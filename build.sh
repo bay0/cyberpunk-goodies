74843f04c1e62c5c0051a95bb3ac9fa23a9d4d28
Bayram <bayram.hendem@qmarketing.de>
2020-12-11 23:43:34 +0100

    adjusted build

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-rm -rf Windows-*
-rm -rf Linux-*
-rm -rf Darwin-*
+function clean {
+    rm -rf Windows-*
+    rm -rf Linux-*
+    rm -rf Darwin-*
+}
+
+clean
 
 mkdir Windows-x64
 env GOOS=windows GOARCH=amd64 go build -v -o ./Windows-x64 .
@@ -14,3 +18,11 @@ env GOOS=linux GOARCH=386 go build -o ./Linux-x32 -v .
 
 mkdir Darwin-x64
 env GOOS=darwin GOARCH=amd64 go build -o ./Darwin-x64 -v .
+
+zip -r Windows-x64.zip Windows-x64
+zip -r Windows-x32.zip Windows-x32
+
+zip -r Linux-x64.zip Linux-x64
+zip -r Linux-x32.zip Linux-x32
+
+zip -r Darwin-x64.zip Darwin-x64
